2fb00034142e06eb7479f4033091fa485861e506
oao519p <gongon739@gmail.com>
2018-12-23 08:48:44 +0000

    finish tasks

--- src/controller.py
+++ src/controller.py
@@ -102,9 +102,9 @@ class SimpleController1(app_manager.RyuApp):
                 priority=3,
                 match=match,
                 actions=actions)
-            # For h2-h1 flow: s3 -> s1 -> h1
+            # For h2-h1 flow: s2 -> s1 -> h1
             match = parser.OFPMatch(
-                in_port=3,
+                in_port=2,
                 eth_type=0x0800,
                 ipv4_src="10.0.0.2",
                 ipv4_dst="10.0.0.1",
@@ -119,7 +119,7 @@ class SimpleController1(app_manager.RyuApp):
 
         # Add forwarding rule in s3
         if msg.datapath.id == 3:
-            # For h2-h1 flow: h2 -> s3 -> s1
+            # For h2-h1 flow: h2 -> s3 -> s2
             match = parser.OFPMatch(
                 in_port=1,
                 eth_type=0x0800,
@@ -127,7 +127,7 @@ class SimpleController1(app_manager.RyuApp):
                 ipv4_dst="10.0.0.1",
                 ip_proto=17,
                 udp_dst=5566)
-            actions = [parser.OFPActionOutput(2)]
+            actions = [parser.OFPActionOutput(3)]
             self.add_flow(
                 datapath=datapath,
                 priority=3,
@@ -148,7 +148,23 @@ class SimpleController1(app_manager.RyuApp):
                 match=match,
                 actions=actions)
 
-    
+        # Add forwarding rule in s2                          
+        if msg.datapath.id == 2:
+             # for h2-h1
+             match = parser.OFPMatch(
+                in_port=2,
+                eth_type=0x0800,
+                ipv4_src="10.0.0.2",
+                ipv4_dst="10.0.0.1",
+                ip_proto=17,
+                udp_dst=5566)
+             actions = [parser.OFPActionOutput(1)]
+             self.add_flow(
+                 datapath=datapath,
+                 priority=3,
+                 match=match,
+                 actions=actions)     
+
     '''
     METHOD : packet_in_handler (@set_ev_cls)
     Handle the packet-in events (DO NOT MODIFY)
